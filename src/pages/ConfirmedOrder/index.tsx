398624e056ccf2321ea0ea96254fa99bab746415
Gustavo Abreu <guustavoabreu99@gmail.com>
2024-01-28 02:39:32 -0300

    Main structure of the last page

--- src/pages/ConfirmedOrder/index.tsx
+++ src/pages/ConfirmedOrder/index.tsx
@@ -1,7 +1,30 @@
 export default function ConfirmedOrder() {
 	return (
 		<div>
-			<h1>ConfirmedOrder</h1>
+			<div>
+				<h1>Uhu! Pedido confirmado</h1>
+				<p>Agora é só aguardar que logo o café chegará até você</p>
+			</div>
+			<div>
+				<div>
+					<div>
+						icon
+						<p>Entrega em </p>
+						<p>Entrega em </p>
+					</div>
+					<div>
+						icon
+						<p>Previsão de entrega</p>
+						<p>20 min - 30 min</p>
+					</div>
+					<div>
+						icon
+						<p>Pagamento na entrega</p>
+						<p>Cartão de Crédito</p>
+					</div>
+				</div>
+				<div></div>
+			</div>
 		</div>
 	);
 }
